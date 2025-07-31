3cabb401cf600bfcbf152b3d16e5783daf714117
AmrElShamy1805 <A.Elshamy06429@student.aast.edu>
2025-07-31 17:07:24 +0300

    more changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/Supermarket-Project/">
+  <HashRouter >
     <App />
   </HashRouter>,
 )
